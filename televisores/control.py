deadcd9101e8fc134749e6836a065d096e04a889
Juan Pablo Gaviria Orozco <141966367+Darkblack595@users.noreply.github.com>
2023-08-27 11:47:49 -0500

    pa

--- televisores/control.py
+++ televisores/control.py
@@ -1,35 +1,28 @@
+from televisores.tv import TV
 class Control:
-    def _init_(self):
-        self.tv = None
+    def _init_(self, tv):
+        TV.tv = self.tv
     
     def turnOn(self):
-        if self.tv is not None:
             self.tv.turnOn()
     def turnOff(self):
-        if self.tv is not None:
             self.tv.turnOff()
     
     def canalUp(self):
-        if self.tv is not None:
             self.tv.canalUp()
     def canalDown(self):
-        if self.tv is not None:
             self.tv.canalDown()
     
     def volumeDown(self):
-        if self.tv is not None:
             self.tv.canalDown()
-    def volumeUp(self):}
-        if self.tv is not None:
+    def volumeUp(self):
             self.tv.volumenUp()
     
     def setCanal(self,canal):
-        if self.tv is not None:
             self.tv.setCanal()
     def setVolumen(self, volumen):
-        if self.tv is not None:
             self.tv.setVolumen()
     
     def enlazar(self, tv):
         self.tv = tv
-        self.tv.setControl(self)
+        TV.tv.setControl(self)
