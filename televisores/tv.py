06ab98a2811bee34cb56023c4686de01e515b4a4
Juan Pablo Gaviria Orozco <141966367+Darkblack595@users.noreply.github.com>
2023-08-27 13:32:02 -0500

    fsdsd

--- televisores/tv.py
+++ televisores/tv.py
@@ -51,13 +51,16 @@ class TV:
         TV.numTV = numTV
 
     def canalUp(self):
-        self.setCanal(self.getCanal() + 1)
+        if self.estado == True and self.canal >=1 and self.canal <= 120:
+            self.canal = self.canal + 1
     def canalDown(self):
-        self.setCanal(self.getCanal() - 1)
+        if self.estado == True and self.canal >=1 and self.canal <= 120:
+            self.canal = self.canal + 1
 
     def volumenUp(self):
-        if self.estado == True:
-            self.setVolumen(self.getVolumen() + 1)
+        if self.estado == True and self.volumen >=0 and self.volumen <= 7:
+            self.volumen = self.volumen + 1
     def volumenDown(self):
-        if self.estado == True:
-            self.setVolumen(self.getVolumen() - 1)
+        if self.estado == True and self.volumen >=0 and self.volumen <= 7:
+            self.volumen = self.volumen + 1
+    
